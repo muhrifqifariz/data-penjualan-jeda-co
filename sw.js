c1028280ed848dc528f341abfdab992da33149fd
muhrifqifariz <57094159+muhrifqifariz@users.noreply.github.com>
2026-07-10 06:26:10 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "catatan-penjualan-v3";
+const CACHE_NAME = "catatan-penjualan-v5";
 const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./logo-header.png"];
 
 self.addEventListener("install", (event) => {
